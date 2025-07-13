409631f5f10ceac94cc98a444949e65dd66ff718
MilkShake <sirapat.sermcheep@gmail.com>
2025-07-13 14:45:04 +0700

    a

--- server.js
+++ server.js
@@ -198,6 +198,7 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
+    console.log(user)
     done(null, user);
   } catch (error) {
     done(error, null);
