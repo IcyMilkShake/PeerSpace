a9499afb15998d57f80ab94ff4485090979a703f
MilkShake <sirapat.sermcheep@gmail.com>
2025-10-16 11:14:45 +0700

    a

--- server.js
+++ server.js
@@ -797,7 +797,7 @@ app.delete('/api/comments/:commentId', isAuthenticated, async (req, res) => {
         post.answeredComment = null;
         await post.save();
         // Revoke credibility from the author of the answer
-        await revokeCredibility(comment.author.toString(), 10);
+        await revokeCredibility(comment.author.toString(), 10, comment);
     }
     const io = req.app.get('socketio');
     io.emit('comment:delete', { commentId, postId, parentCommentId });
