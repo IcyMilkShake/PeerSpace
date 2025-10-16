01bd83d97c66581700cb2fbcb2461d7d81d6a112
MilkShake <sirapat.sermcheep@gmail.com>
2025-10-16 12:11:25 +0700

    a

--- server.js
+++ server.js
@@ -2384,8 +2384,8 @@ app.post('/api/comments/:commentId/unmark-answer', isAuthenticated, async (req, 
     }
 
     if (post.answeredComment && post.answeredComment.toString() === commentId) {
-      console.log(comment.author, post.author)
-      if (comment.author !== post.author) {
+      console.log(comment, post)
+      if (comment !== post) {
         post.answeredComment = null;
         await post.save();
         await revokeCredibility(comment.author, 10);
